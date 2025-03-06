1016db54d9e8d715cecf1d0f63b6b563f3898e90
Agatha Sates <satesagatha@gmail.com>
2025-03-06 15:31:38 -0300

    Add project files.

---
+++ HelloWord.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWord.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+
+            Console.WriteLine("Sejam bem-vindos a Academia do Programador 2025!");
+
+            Console.ReadLine();
+        }
+    }
+}
