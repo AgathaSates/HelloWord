fe646103a8afdd4d7745640280ed024a34f3762c
Agatha Sates <satesagatha@gmail.com>
2025-03-06 15:43:57 -0300

    adicionado linha teste do commit

--- HelloWord.ConsoleApp/Program.cs
+++ HelloWord.ConsoleApp/Program.cs
@@ -8,6 +8,8 @@
 
             Console.WriteLine("Sejam bem-vindos a Academia do Programador 2025!");
 
+            Console.WriteLine("teste commit");
+
             Console.ReadLine();
         }
     }
